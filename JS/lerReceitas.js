497bc6643d484252512f4a50505833d4e15dc345
Yago <yagodevtech@gmail.com>
2025-02-24 20:33:08 -0300

    feito o projeto da vovo

---
+++ JS/lerReceitas.js
@@ -0,0 +1,25 @@
+const recipes = JSON.parse(localStorage.getItem('recipes')) || [];
+
+
+const listaReceitas = document.getElementById("listaReceitas");
+
+if (recipes.length > 0) {
+    recipes.forEach(function(recipe) {
+        const divReceita = document.createElement('div');
+        divReceita.classList.add('card' , 'container','mt-2', 'mb-2');
+        divReceita.style.backgroundColor = 'white';
+        divReceita.style.padding = '0px'
+        divReceita.innerHTML = `
+        <div class="card-body">
+            <h5 class="card-title">${recipe.nome}</h5>
+            <p><strong>Nome da receita:</strong> ${recipe.titulo}</p>
+            <p><strong>Ingredientes:</strong> ${recipe.ingredientes}</p>
+            <p><strong>Modo de Preparo:</strong> ${recipe.instrucoes}</p>
+        </div>
+    `;
+       
+        listaReceitas.appendChild(divReceita);
+    });
+} else {
+    listaReceitas.innerHTML = "<p>Nenhuma receita encontrada.</p>";
+};
---
+++ JS/saveRecipe.js
@@ -0,0 +1,31 @@
+document.getElementById('recipeForm').addEventListener("submit" , function(event) {
+event.preventDefault();
+
+
+
+const nomePessoa = document.querySelector("#nomePessoa").value;
+const ingredientes = document.querySelector('#ingredientes').value;
+const titulo = document.querySelector('#titulo').value;
+const instrucoes = document.querySelector('#instrucoes').value;
+const imgReceita = document.querySelector('#imgReceita').file;
+
+
+const recipe = {
+    nome: nomePessoa,
+    ingredientes: ingredientes,
+    titulo: titulo,
+    instrucoes: instrucoes,
+    imagem: imgReceita
+};
+
+let recipes = JSON.parse(localStorage.getItem('recipes')) || [];
+
+recipes.push(recipe);
+
+localStorage.setItem('recipes', JSON.stringify(recipes));
+
+document.getElementById("recipeForm").reset();
+
+alert("Receita salva com sucesso!")
+
+});
